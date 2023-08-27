0b3f087dce1832f5fc65ee42d51481b1709e9d9f
Vinícius Moreira Hungaro <hungaro545@gmail.com>
2023-08-27 11:17:12 -0300

    Created main functions  categories and subcategories

--- wiseInvestApi/src/Repositories/categoriesRepositories.js
+++ wiseInvestApi/src/Repositories/categoriesRepositories.js
@@ -1,10 +1,11 @@
 const db = require('../db/dbMySql');
 
+// Category
 const listCategoriesRepository = async () => {
   try {
     let result = await db.query(`SELECT *
                                 FROM dbo.Categories;`);
-    return result[0][0];
+    return result[0];
   } catch (error) {
     return error;
   }
@@ -46,8 +47,65 @@ const detailsCategoryRepository = async (id) => {
 
 const deleteRepository = async (id) => {
   try {
-    const result = await db.query(`DELETE dbo.Categories 
+    const result = await db.query(`DELETE FROM dbo.Categories 
                                    WHERE ID_Category = ${id}`);
+    
+    return [result[0]["affectedRows"]];
+  } catch (error) {
+    return error;
+  }
+}
+
+// Subcategory
+const listSubcategoriesRepository = async () => {
+  try {
+    let result = await db.query(`SELECT *
+                                FROM dbo.Subcategories;`);
+    return result[0];
+  } catch (error) {
+    return error;
+  }
+}
+
+const registerSubcategoryRepository = async (data) => {
+  try {
+    const result = await db.query("INSERT INTO dbo.Subcategories(Description_Subcategory, ID_Category) VALUES (?,?)", [data.Description_Subcategory, data.ID_Category]);
+    
+    return [result[0]["affectedRows"], result[0]["insertId"]];
+  } catch (error) {
+    return error;
+  }
+}
+
+const updateSubcategoryRepository = async (id, data) => {
+  try {
+    const result = await db.query(`UPDATE Subcategories 
+                                   SET Description_Subcategory = ?
+                                      ,ID_Category             = ?
+                                   WHERE ID_Subcategory = ?;`, [data.Description_Subcategory, data.ID_Category, id]);
+
+    
+    return [result[0]["affectedRows"]];
+  } catch (error) {
+    return error;
+  }
+}
+
+const detailsSubcategoryRepository = async (id) => {
+  try {
+    const result = await db.query(`SELECT *
+                                   FROM dbo.Subcategories
+                                   WHERE ID_Subcategory = ${id}`);
+    return result[0];
+  } catch (error) {
+    return error;
+  }
+}
+
+const deleteSubcategoryRepository = async (id) => {
+  try {
+    const result = await db.query(`DELETE FROM dbo.Subcategories 
+                                   WHERE ID_Subcategory = ${id}`);
     
     return [result[0]["affectedRows"]];
   } catch (error) {
@@ -60,5 +118,11 @@ module.exports= {
   registerRepository,
   updateRepository,
   detailsCategoryRepository,
-  deleteRepository
+  deleteRepository,
+
+  listSubcategoriesRepository,
+  registerSubcategoryRepository,
+  updateSubcategoryRepository,
+  detailsSubcategoryRepository,
+  deleteSubcategoryRepository
 }
--- wiseInvestApi/src/Routes/categoriesRoutes.js
+++ wiseInvestApi/src/Routes/categoriesRoutes.js
@@ -5,10 +5,17 @@ const { isValidToken } = require("../Ultils/Ultils");
 const categoryController = require("../Controllers/categoriesController");
 
 // categories
-// router.post("/login",  categoryController.);
-router.post("/register", categoryController.registerController);
-// router.post("/register", isValidToken, categoryController.registerController);
-router.put("/update/:id", categoryController.updateController);
-router.get("/verify/:id", categoryController.verifyUserController);
+router.get("/listCategories",  categoryController.listCategoriesController);
+router.get("/detailsCategory/:id", categoryController.detailsCategoryController);
+router.post("/addCategory", categoryController.registerController);
+router.put("/updateCategory/:id", categoryController.updateController);
+router.delete("/deleteCategory/:id", categoryController.deleteCategoryController);
+
+// categories
+router.get("/subcategories/listSubcategories",  categoryController.listSubcategoriesController);
+router.get("/subcategories/detailsSubcategory/:id", categoryController.detailsSubcategoryController);
+router.post("/subcategories/addSubcategory", categoryController.registerSubcategoryController);
+router.put("/subcategories/updateSubcategory/:id", categoryController.updateSubcategoryController);
+router.delete("/subcategories/deleteSubcategory/:id", categoryController.deleteSubcategoryController);
 
 module.exports = router;
