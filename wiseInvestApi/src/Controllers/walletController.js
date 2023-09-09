d30eae0218913dccbf4410c2774f959fb92a6cc8
Vinícius Moreira Hungaro <hungaro545@gmail.com>
2023-09-09 00:24:13 -0300

    Wallet Consolidated

--- wiseInvestApi/src/Controllers/walletController.js
+++ wiseInvestApi/src/Controllers/walletController.js
@@ -112,10 +112,88 @@ const deleteWalletController = async (req, res, next) => {
   }
 }
 
+const verifyConsolidateWalletController = async (req, res, next) => {
+  const _idUser = req.params.idUser;
+  const _idWallet = req.params.idWallet;
+  
+  try {
+    const response = await walletServices.verifyConsolidateWalletService(_idUser, _idWallet);
+    
+    return res.status(200).send(response);
+  } catch (e) {
+    const message = JSON.stringify({"title": e.name, "": e.message});
+    return res.status(e.code).send(message);
+  }
+}
+
+const consolidateWalletController = async (req, res, next) => {
+
+  const _idUser = req.params.idUser;
+  const _idWallet = req.params.idWallet;
+  
+  try {
+    const response = await walletServices.consolidateWalletService(_idUser, _idWallet);
+    if (response !== true) {
+      const exception = new Error(response.message);
+      exception.code = response.code;
+      throw exception;
+    }
+    
+    return res.status(201).send(response);
+  } catch (e) {
+    const message = JSON.stringify({"title": e.name, "": e.message});
+    return res.status(e.code).send(message);
+  }
+}
+
+const getConsolidatedWalletController = async (req, res, next) => {
+  const _idUser = req.params.idUser;
+  const _idWallet = req.params.idWallet;
+
+  try {
+    const response = await walletServices.getConsolidatedWalletService(_idUser, _idWallet);
+
+    if (response.code > 0 && response.title === 'Error') {  
+      const exception = new Error(response.message);
+      exception.code = response.code;
+      throw exception;
+    }
+
+    res.status(200).send(response);
+  } catch (e) {
+    const message = JSON.parse({"title": e.name, "menssage": e.message});
+    return res.status(e.code).send(message);
+  }
+}
+
+const deleteConsolidatedWalletController = async (req, res, next) => {
+  const {idUser, idWallet } = req.params;
+  
+  try {
+    const response = await walletServices.deleteConsolidatedWalletService(idUser, idWallet);
+    if (response.code > 0 && response.title === 'Error') {
+      const exception = new Error(response.message);
+      exception.code = response.code;
+      throw exception;
+    }
+
+    if (response[0] === 1)
+      return res.status(200).send(true);
+    
+  } catch (e) {
+    const message = {"title": e.name, "Message:": e.message };
+    return res.status(e.code).send(`${JSON.stringify(message)}`);
+  }
+}
+
 module.exports = { 
   listWalletsController,
   registerWalletController,
   updateWalletController,
   detailsWalletController,
-  deleteWalletController
+  deleteWalletController,
+  consolidateWalletController,
+  getConsolidatedWalletController,
+  verifyConsolidateWalletController,
+  deleteConsolidatedWalletController
 };
--- wiseInvestApi/src/Repositories/walletRepositories.js
+++ wiseInvestApi/src/Repositories/walletRepositories.js
@@ -56,10 +56,78 @@ const deleteWalletRepository = async (id) => {
   }
 }
 
+const verifyConsolidateWalletRepository = async (idUser, idWallet) => {
+  try {
+    const result = await db.query(`SELECT 1
+                                   FROM dbo.Wallet_Consolidated
+                                   WHERE ID_User = ${idUser}
+                                     AND ID_Wallet = ${idWallet}`);
+
+    return result[0];
+  } catch (error) {
+    return error;
+  }
+}
+
+const createConsolidateWalletRepository = async (idUser, idWallet) => {
+  try {
+    const result = await db.query("INSERT INTO dbo.Wallet_Consolidated(DT_Consolidated, ID_User, ID_Wallet) VALUES (?,?,?)", 
+                                 [new Date(), idUser, idWallet]);
+    
+    return [result[0]["affectedRows"], result[0]["insertId"]];
+  } catch (error) {
+    return error;
+  }
+}
+
+const createStocksConsolidateWalletRepository = async (data) => {
+  try {
+    const result = await db.query("INSERT INTO dbo.Stocks_Wallet_Consolidated(CD_Stocks, Average_Price, Total_Amount, ID_Wallet_Consolidated) VALUES (?,?,?,?)", 
+                                 [data.CD_Stocks, data.Average_Price, data.Total_Amount, data.ID_Wallet_Consolidated]);
+    
+    return [result[0]["affectedRows"], result[0]["insertId"]];
+  } catch (error) {
+    return error;
+  }
+}
+
+const getConsolidatedWalletRepository = async (idUser, idWallet) => {
+  try {
+    const result = await db.query(`SELECT Stocks_Wallet_Consolidated.CD_Stocks, 
+                                          Stocks_Wallet_Consolidated.Average_Price,
+                                          Stocks_Wallet_Consolidated.Total_Amount
+                                   FROM dbo.Wallet_Consolidated
+                                        INNER JOIN Stocks_Wallet_Consolidated
+                                        ON Stocks_Wallet_Consolidated.ID_Wallet_Consolidated = Wallet_Consolidated.ID_Wallet_Consolidated
+                                   WHERE Wallet_Consolidated.ID_User   = ${idUser}
+                                     AND Wallet_Consolidated.ID_Wallet = ${idWallet};`);
+                                 
+    return result[0];
+  } catch (error) {
+    return error;
+  }
+}
+
+const deleteConsolidatedWalletRepository = async (idUser, idWallet) => {
+  try {
+    const result = await db.query(`DELETE FROM dbo.Wallet_Consolidated 
+                                   WHERE Wallet_Consolidated.ID_User   = ${idUser}
+                                     AND Wallet_Consolidated.ID_Wallet = ${idWallet};`);
+    return [result[0]["affectedRows"]];
+  } catch (error) {
+    return error;
+  }
+}
+
 module.exports= {
   listWalletsRepository,
   registerWalletRepository,
   updateWalletRepository,
   detailsWalletRepository,
-  deleteWalletRepository
+  deleteWalletRepository,
+  createConsolidateWalletRepository,
+  createStocksConsolidateWalletRepository,
+  getConsolidatedWalletRepository,
+  verifyConsolidateWalletRepository,
+  deleteConsolidatedWalletRepository
 }
