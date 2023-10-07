3ef7c801ad14e59f66865708534d12106085b9d8
Vinícius Moreira Hungaro <hungaro545@gmail.com>
2023-10-07 00:06:50 -0300

    login screen

---
+++ wiseinvestsite/src/pages/Dashboard.jsx
@@ -0,0 +1,16 @@
+import { Header } from "../components/Header/Header";
+
+export const Dashboard = () => {
+	return(
+		<>
+            <div className="container">
+                <Header />
+                <main>
+                    <span>
+                        Teste 2
+                    </span>
+                </main>
+            </div>
+		</>
+	)
+}
