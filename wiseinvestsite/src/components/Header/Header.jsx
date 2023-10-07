ddaa6c4b0a141065bff5f0972996fb1649dfc976
Vinícius Moreira Hungaro <hungaro545@gmail.com>
2023-10-07 00:06:50 -0300

    login screen

---
+++ wiseinvestsite/src/components/Header/Header.jsx
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom";
+
+export const Header = () => {
+  return (
+    <div className="containerHeader">
+      <div className="logo">
+        <Link to={"/dashboard"}>
+          <h1>Wise Invest</h1>
+        </Link>
+      </div>
+
+      <nav>
+        {/* Todos  */}
+        <Link to={"/perfil"}>Perfil</Link>
+        {/* 1 */}
+        <Link to={"/users"}>Usuarios</Link>{" "}
+        {/* Controle de acessos e usuarios */}
+        {/* 2 */}
+        <Link to={"/myRecommendation"}>Minhas Recomendações</Link>{" "}
+        {/* Cadastro de Recomendações - Interno button para cadastro */}
+        {/* 3 */}
+        <Link to={"/Wallets"}>Carteiras</Link>{" "}
+        {/* Cadastro de carteira - Carteira Multiplas */}
+        <Link to={"/brokerageNotes"}>Nova Nota de Corretagem</Link>{" "}
+        {/* Cadastro de notas de corretagem */}
+        <Link to={"/recommendations"}>Recomendações</Link>{" "}
+        {/* Recomendações de analistas */}
+        {/* Pensar
+                Rentabilidade
+                Calendario de Dividendos  */}
+        {/* 4 */}
+        {/* Cadastro de carteira - Carteira Unica */}
+        {/* Cadastro de notas de corretagem */}
+        <Link to={"/detailedProfitability"}>Rentabilidade Detalhada</Link>
+        {/* Rentabilidade */}
+      </nav>
+    </div>
+  );
+};
