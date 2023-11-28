3887ecb7458e2a4f2417ddca81f68f9645e9f821
Vinícius Moreira Hungaro <hungaro545@gmail.com>
2023-11-27 21:06:27 -0300

    First wallet screens created

---
+++ wiseinvestsite/src/pages/Wallets/Wallets.jsx
@@ -0,0 +1,64 @@
+import Button from "../../components/Button/Button";
+
+import './wallets.css';
+
+export const Wallets = () => {
+	
+	const handlerClickSeeDetails = () => {
+	
+	}
+
+	return (
+        <>
+            <div className='container conteinerWallets'>
+                <div className='titleWallets'>
+                    <h1 className="logo">Carteiras</h1>
+                    <Button className="btn">Adicionar Carteira</Button>
+                </div>
+                <div className='conteinerItemWallets'>
+                    <div className='card itemWallet'>
+                        <h1>Carteira Principal</h1>
+						<p>Carteira dos meus principais ativos, Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.</p>
+                        <Button className="btn">Ver Detalhes</Button>
+                    </div>
+
+					<div className='card itemWallet'>
+                        <h1>Carteira Principal</h1>
+						<p>Carteira dos meus principais ativos, Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.</p>
+                        <Button className="btn">Ver Detalhes</Button>
+                    </div>
+
+					<div className='card itemWallet'>
+                        <h1>Carteira Principal</h1>
+						<p>Carteira dos meus principais ativos, Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.</p>
+                        <Button className="btn">Ver Detalhes</Button>
+                    </div>
+
+					<div className='card itemWallet'>
+                        <h1>Carteira Principal</h1>
+						<p>Carteira dos meus principais ativos, Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.</p>
+                        <Button className="btn">Ver Detalhes</Button>
+                    </div>
+
+					<div className='card itemWallet'>
+                        <h1>Carteira Principal</h1>
+						<p>Carteira dos meus principais ativos, Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.</p>
+                        <Button className="btn">Ver Detalhes</Button>
+                    </div>
+					
+					<div className='card itemWallet'>
+                        <h1>Carteira Principal</h1>
+						<p>Carteira dos meus principais ativos, Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.</p>
+                        <Button className="btn">Ver Detalhes</Button>
+                    </div>
+					
+					<div className='card itemWallet'>
+                        <h1>Carteira Principal</h1>
+						<p>Carteira dos meus principais ativos, Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.</p>
+                        <Button className="btn">Ver Detalhes</Button>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
