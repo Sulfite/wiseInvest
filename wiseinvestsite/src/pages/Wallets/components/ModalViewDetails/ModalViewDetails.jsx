3a977f933a47ee781d23c2b4763873f9306b0d09
Vinícius Moreira Hungaro <hungaro545@gmail.com>
2023-11-28 23:24:38 -0300

    Adjustments to the new modals created on the wallets page

--- wiseinvestsite/src/pages/Wallets/components/ModalViewDetails/ModalViewDetails.jsx
+++ wiseinvestsite/src/pages/Wallets/components/ModalViewDetails/ModalViewDetails.jsx
@@ -1,37 +1,58 @@
 import "./modalViewDetails.css";
 
 import { ModalComponent } from "../../../../components/Modal/ModalComponent";
+import Button from "../../../../components/Button/Button";
+import { IoMdClose } from "react-icons/io";
+import { useState } from "react";
+
+const customStyles = {
+    content: {
+        top: "50%",
+        left: "50%",
+        right: "auto",
+        bottom: "auto",
+        marginRight: "-50%",
+        transform: "translate(-50%, -50%)",
+        width: "50%",
+    },
+};
 
 export const ModalViewDetailsWallet = ({ open, close, idWallet }) => {
+    const [info, setInfo] = useState({
+        titleWallet: "Carteira Principal",
+        description:
+            "Carteira dos meus principais ativos, Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.",
+    });
+
     // funcao para realizar chamada de detalhes da carteira
+    // const componentDidMount = () => {
+    // setInfo({
+    //     titleWallet: "Carteira Principal",
+    //     description: "Carteira dos meus principais ativos, Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker."
+    // })
+    // }
 
     return (
         <ModalComponent
             open={open}
             // onAfterOpen={afterOpenModal}
             close={close}
-            // style={customStyles}
+            customStyles={customStyles}
             // contentLabel='Example Modal'
         >
-            <h1>Carteira Principal</h1>
+            <div className='containerModalTitle'>
+                <h1 className="logo">{info.titleWallet}</h1>
+                <Button
+                    className='nBtn'
+                    onClick={close}
+                >
+                    <IoMdClose />
+                </Button>
+            </div>
 
-            <button onClick={close}>close</button>
-
-            <h2>{idWallet}</h2>
-
-            <p>
-                Carteira dos meus principais ativos, Lorem Ipsum é simplesmente
-                uma simulação de texto da indústria tipográfica e de impressos,
-                e vem sendo utilizado desde o século XVI, quando um impressor
-                desconhecido pegou uma bandeja de tipos e os embaralhou para
-                fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não
-                só a cinco séculos, como também ao salto para a editoração
-                eletrônica, permanecendo essencialmente inalterado. Se
-                popularizou na década de 60, quando a Letraset lançou decalques
-                contendo passagens de Lorem Ipsum, e mais recentemente quando
-                passou a ser integrado a softwares de editoração eletrônica como
-                Aldus PageMaker.
-            </p>
+            <div className='containerModalBody'>
+                <p>{info.description}</p>
+            </div>
         </ModalComponent>
     );
 };
